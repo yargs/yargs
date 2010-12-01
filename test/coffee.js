4e6c7aba6374ac9ebc6259ecf91f13af7bce40e3
James Halliday <substack@gmail.com>
2010-12-01 04:31:19 +0000

    test for coffee usage

---
+++ test/coffee.js
@@ -0,0 +1,24 @@
+// coffee script style arguments where process.argv is .splice(2)'d
+var fs = require('fs');
+var Script = process.binding('evals').Script;
+var Hash = require('traverse/hash');
+
+exports.coffee = function (assert) {
+    fs.readFile(__dirname + '/../lib/optimist.js', function (err, src) {
+        if (err) assert.fail(err);
+        var context = {
+            require : require,
+            module : { exports : {} },
+            process : Hash.merge(process, {
+                argv : [ 'meow', '--x=10' ],
+            }),
+        };
+        context.exports = context.module.exports;
+        Script.runInNewContext(src.toString(), context);
+        
+        var argv = context.module.exports.argv;
+        assert.eql(argv.x, 10);
+        assert.eql(argv._, ['meow']);
+        
+    });
+};
