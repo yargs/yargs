dfdaa5d84d2c24e933dd410e1542a3e41a4a51bd
James Halliday <mail@substack.net>
2013-05-18 11:56:50 -0700

    more complete dash test

--- test/dash.js
+++ test/dash.js
@@ -2,8 +2,10 @@ var optimist = require('../index');
 var test = require('tap').test;
 
 test('-', function (t) {
-    t.plan(3);
+    t.plan(5);
     t.equal(optimist.parse([ '-n', '-' ]).n, '-');
+    t.equal(optimist.parse([ '-n', '-' ])['-'], undefined);
     t.same(optimist.parse([ '-' ])._, [ '-' ]);
-    t.same(optimist.parse([ '-f-' ]).f, [ '-' ]);
+    t.equal(optimist.parse([ '-f-' ]).f, '-');
+    t.equal(optimist.parse([ '-f-' ])['-'], undefined);
 });
