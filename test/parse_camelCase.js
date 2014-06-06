66f12c82ba3c943e4de8ca862980e835da8ecb3a
James Nylen <jnylen@gmail.com>
2014-06-06 10:22:42 -0500

    Fix a couple of bad interactions between aliases and defaults
    - Alias + boolean + default is broken (#7)
    - Default values and options with dashes not working (#20)

--- test/parse_camelCase.js
+++ test/parse_camelCase.js
@@ -34,6 +34,41 @@ describe('parse', function () {
             result.should.have.property('someOption').that.is.a('boolean').and.is.true;
         });
 
+        it('should provide defaults of options with dashes as camelCase properties', function() {
+            var result = yargs()
+                .option('some-option', {
+                    describe : 'some option',
+                    default  : 'asdf'
+                })
+                .parse([ ]);
+
+            result.should.have.property('someOption', 'asdf');
+        });
+
+        it('should provide aliases of options with dashes as camelCase properties', function() {
+            var result = yargs()
+                .option('some-option', {
+                    alias    : 'o',
+                    describe : 'some option',
+                    default  : 'asdf'
+                })
+                .parse([ ]);
+
+            result.should.have.property('o', 'asdf');
+        });
+
+        it('should provide aliases of options with dashes as camelCase properties', function() {
+            var result = yargs()
+                .option('o', {
+                    alias    : 'some-option',
+                    describe : 'some option',
+                    default  : 'asdf'
+                })
+                .parse([ ]);
+
+            result.should.have.property('someOption', 'asdf');
+        });
+
         it('should provide aliases with dashes as camelCase properties', function() {
             var result = yargs()
                 .option('o', {
