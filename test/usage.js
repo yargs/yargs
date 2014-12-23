bcea78157d16bac04cb8a011192d52b3b4c71e22
bcoe <bencoe@gmail.com>
2014-12-23 12:05:35 -0600

    fixes https://github.com/chevex/yargs/issues/71

--- test/usage.js
+++ test/usage.js
@@ -723,7 +723,7 @@ describe('usage', function () {
             ]);
         });
     });
-    
+
     describe('exitProcess', function () {
         it('should not call process.exit on error if disabled', function () {
             var opts = {
@@ -750,6 +750,15 @@ describe('usage', function () {
         yargs.rebase('/home/chevex', '/home/chevex/foo/bar/baz').should.equal('./foo/bar/baz');
         yargs.rebase('/home/chevex/foo/bar/baz', '/home/chevex').should.equal('../../..');
         yargs.rebase('/home/chevex/foo', '/home/chevex/pow/zoom.txt').should.equal('../pow/zoom.txt');
+    });
+
+    // Fixes: https://github.com/chevex/yargs/issues/71
+    it('should not raise an exception if help called on empty arguments', function() {
+      var r = checkUsage(function () {
+        return yargs([]).usage('foo').help();
+      });
+
+      r.result.should.match(/foo/);
     });
 
     function checkUsage (f) {
