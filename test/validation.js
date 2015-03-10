7c7f9022d238bf82911c22b95d8168b3a0ffada1
Raine Virta <raine.virta@gmail.com>
2015-03-11 01:18:49 +0200

    reindent test/validation.js

--- test/validation.js
+++ test/validation.js
@@ -12,51 +12,51 @@ describe('validation tests', function () {
     describe('implies', function() {
         it("fails if '_' populated, and implied argument not set", function(done) {
             var argv = yargs(['cat'])
-              .implies({
-                1: 'foo'
-              })
-              .fail(function(msg) {
-                msg.should.match(/Implications failed/);
-                return done();
-              })
-              .argv;
+                .implies({
+                    1: 'foo'
+                })
+                .fail(function(msg) {
+                    msg.should.match(/Implications failed/);
+                    return done();
+                })
+                .argv;
         });
 
         it("fails if key implies values in '_', but '_' is not populated", function(done) {
             var argv = yargs(['--foo'])
-              .boolean('foo')
-              .implies({
-                'foo': 1
-              })
-              .fail(function(msg) {
-                msg.should.match(/Implications failed/);
-                return done();
-              })
-              .argv;
+                .boolean('foo')
+                .implies({
+                    'foo': 1
+                })
+                .fail(function(msg) {
+                    msg.should.match(/Implications failed/);
+                    return done();
+                })
+                .argv;
         });
 
         it("fails if --no-foo's implied argument is not set", function(done) {
             var argv = yargs([])
-              .implies({
-                '--no-bar': 'foo'
-              })
-              .fail(function(msg) {
-                msg.should.match(/Implications failed/);
-                return done();
-              })
-              .argv;
+                .implies({
+                    '--no-bar': 'foo'
+                })
+                .fail(function(msg) {
+                    msg.should.match(/Implications failed/);
+                    return done();
+                })
+                .argv;
         });
 
         it("fails if a key is set, along with a key that it implies should not be set", function(done) {
             var argv = yargs(['--bar', '--foo'])
-              .implies({
-                'bar': '--no-foo'
-              })
-              .fail(function(msg) {
-                msg.should.match(/Implications failed/);
-                return done();
-              })
-              .argv;
+                .implies({
+                    'bar': '--no-foo'
+                })
+                .fail(function(msg) {
+                    msg.should.match(/Implications failed/);
+                    return done();
+                })
+                .argv;
         });
     });
 
