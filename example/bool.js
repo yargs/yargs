82c7a4e2ba300abae72c1283b0bb37aed74d1fca
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2017-03-05 12:52:26 -0300

    use console.log instead of util.print, fix #813

--- example/bool.js
+++ example/bool.js
@@ -3,7 +3,7 @@ var util = require('util');
 var argv = require('yargs').argv;
 
 if (argv.s) {
-    util.print(argv.fr ? 'Le chat dit: ' : 'The cat says: ');
+    console.log(argv.fr ? 'Le chat dit: ' : 'The cat says: ');
 }
 console.log(
     (argv.fr ? 'miaou' : 'meow') + (argv.p ? '.' : '')
