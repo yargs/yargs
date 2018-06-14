a6f2021bda39c0b22cd7076211ad7ef1433337cc
Aori Nevo <aori.nevo@bloomingdales.com>
2018-06-13 21:51:18 -0400

    I-1150: Add Unit tests for context reducers and action creators.

--- test/reducers/middleware.spec.js
+++ test/reducers/middleware.spec.js
@@ -7,23 +7,19 @@ describe('middleware reducer', () => {
   const { getMiddleware, addMiddleware, resetMiddleware } = require('../../actions/middleware')
   const middlewareReducer = require('../../reducers/middleware')
   it('should get middleware', () => {
-    middlewareReducer({
-      middleware: []
-    }, getMiddleware()).middleware.should.have.length(0)
+    middlewareReducer([], getMiddleware()).should.have.length(0)
   })
 
   it('should add middleware', () => {
-    middlewareReducer(undefined, addMiddleware(['callback1', 'callback2'])).middleware.should.have.length(2)
-    middlewareReducer(undefined, addMiddleware({})).middleware.should.have.length(1)
+    middlewareReducer(undefined, addMiddleware(['callback1', 'callback2'])).should.have.length(2)
+    middlewareReducer(undefined, addMiddleware({})).should.have.length(1)
   })
 
   it('should reset middleware', function () {
-    middlewareReducer({ middleware: ['callback1', 'callback2'], someOtherKey: 'test' }, resetMiddleware()).middleware.should.have.length(0)
+    middlewareReducer(['callback1', 'callback2'], resetMiddleware()).should.have.length(0)
   })
 
   it('should return state by default', function () {
-    middlewareReducer({
-      middleware: []
-    }).middleware.should.have.length(0)
+    middlewareReducer([]).should.have.length(0)
   })
 })
