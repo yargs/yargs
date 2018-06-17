0a8d3dadaf12407288c030f4b7e1fbaaac3e7f3c
Aori Nevo <aori.nevo@bloomingdales.com>
2018-06-17 17:05:58 -0400

    I-1150: Setup for Phase 2 integration of redux into usage.js

--- actions/usage.js
+++ actions/usage.js
@@ -1,6 +1,8 @@
 const SET_SHOW_HELP_ON_FAIL = 'SET_SHOW_HELP_ON_FAIL'
 const GET_SHOW_HELP_ON_FAIL = 'GET_SHOW_HELP_ON_FAIL'
 const RESET_USAGE = 'RESET_USAGE'
+const FREEZE_USAGE = 'FREEZE_USAGE'
+const UNFREEZE_USAGE = 'UNFREEZE_USAGE'
 
 function setShowHelpOnFail (enabled, message) {
   return {
@@ -22,11 +24,27 @@ function resetUsage () {
   }
 }
 
+function freezeUsage () {
+  return {
+    type: FREEZE_USAGE
+  }
+}
+
+function unfreezeUsage () {
+  return {
+    type: UNFREEZE_USAGE
+  }
+}
+
 module.exports = {
   setShowHelpOnFail,
   getShowHelpOnFail,
   resetUsage,
+  freezeUsage,
+  unfreezeUsage,
   SET_SHOW_HELP_ON_FAIL,
   GET_SHOW_HELP_ON_FAIL,
-  RESET_USAGE
+  RESET_USAGE,
+  FREEZE_USAGE,
+  UNFREEZE_USAGE
 }
