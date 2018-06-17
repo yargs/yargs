4b3f96cf74b4343b9ec052bb006598e302e5993f
Aori Nevo <aori.nevo@bloomingdales.com>
2018-06-17 19:26:28 -0400

    I-1150: Move usageDisabled to usageReducer.

--- reducers/usage.js
+++ reducers/usage.js
@@ -1,8 +1,13 @@
-const {SET_SHOW_HELP_ON_FAIL, RESET_USAGE, FREEZE_USAGE, UNFREEZE_USAGE, SET_FAILURE_OUTPUT} = require('../actions/usage')
+const {SET_SHOW_HELP_ON_FAIL, SET_USAGE_EPILOG, SET_USAGE_DISABLED, RESET_USAGE, FREEZE_USAGE, UNFREEZE_USAGE, SET_FAILURE_OUTPUT} = require('../actions/usage')
 const initialState = {
   failMessage: null,
   showHelpOnFail: true,
   failureOutput: false,
+  epilog: undefined,
+  usageDisabled: false,
+  usages: [],
+  examples: [],
+  commands: [],
   frozen: {}
 }
 
@@ -23,11 +28,24 @@ function setFailureOutput (state = initialState, value) {
   return Object.assign({}, state, { failureOutput: value })
 }
 
+function setUsageEpilog (state = initialState, value) {
+  return Object.assign({}, state, { epilog: value })
+}
+
+function setUsageDisabled (state = initialState, value) {
+  return Object.assign({}, state, { usageDisabled: value })
+}
+
 function resetUsage (state = initialState) {
   return {
     failMessage: null,
     showHelpOnFail: true,
     failureOutput: false,
+    epilog: undefined,
+    usageDisabled: false,
+    usages: [],
+    examples: [],
+    commands: [],
     frozen:
       {}
   }
@@ -37,7 +55,8 @@ function freezeUsage (state = initialState) {
   return Object.assign({}, state, {
     frozen: {
       failMessage: state.failMessage,
-      failureOutput: state.failureOutput
+      failureOutput: state.failureOutput,
+      epilog: state.epilog
     }
   })
 }
@@ -52,6 +71,10 @@ module.exports = function usageReducer (state = initialState, action = {}) {
       return setShowHelpOnFail(state, action.enabled, action.message)
     case SET_FAILURE_OUTPUT:
       return setFailureOutput(state, action.value)
+    case SET_USAGE_EPILOG:
+      return setUsageEpilog(state, action.value)
+    case SET_USAGE_DISABLED:
+      return setUsageDisabled(state, action.value)
     case RESET_USAGE:
       return resetUsage(state)
     case FREEZE_USAGE:
