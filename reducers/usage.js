d10506974b457727d60712703e35b61d66a69c72
Aori Nevo <aori.nevo@bloomingdales.com>
2018-06-17 16:40:35 -0400

    I-1150: Complete phase 1 redux integration in usage

--- reducers/usage.js
+++ reducers/usage.js
@@ -4,18 +4,17 @@ const initialState = {
   showHelpOnFail: true
 }
 
-function setShowHelpOnFail (state = initialState) {
-  let {enabled, message} = state
+function setShowHelpOnFail (state = initialState, enabled, message) {
   if (typeof enabled === 'string') {
     message = enabled
     enabled = true
   } else if (typeof enabled === 'undefined') {
     enabled = true
   }
-  return {
+  return Object.assign({}, state, {
     failMessage: message,
     showHelpOnFail: enabled
-  }
+  })
 }
 
 function getShowHelpOnFail (state = initialState) {
@@ -23,13 +22,13 @@ function getShowHelpOnFail (state = initialState) {
 }
 
 function resetUsage (state = initialState) {
-  return Object.assign({}, state, {failMessage: null})
+  return Object.assign({}, state, {failMessage: null, showHelpOnFail: true})
 }
 
 module.exports = function usageReducer (state = initialState, action = {}) {
   switch (action.type) {
     case SET_SHOW_HELP_ON_FAIL:
-      return setShowHelpOnFail(state)
+      return setShowHelpOnFail(state, action.enabled, action.message)
     case GET_SHOW_HELP_ON_FAIL:
       return getShowHelpOnFail(state)
     case RESET_USAGE:
