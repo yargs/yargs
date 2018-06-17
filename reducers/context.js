b71cf427593b0ae2436f5699c24cc80f934492b8
Aori Nevo <aori.nevo@bloomingdales.com>
2018-06-17 15:57:54 -0400

    I-1150: Fix context reducers conflicts.

--- reducers/context.js
+++ reducers/context.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-const {GET_CONTEXT, INCREMENT_CONTEXT_RESETS} = require('../actions/context')
-=======
 const {GET_CONTEXT, INCREMENT_CONTEXT_RESETS, ADD_FILE_TO_CONTEXT} = require('../actions/context')
->>>>>>> I-1150: Add redux state management for usage.js.
 const initialState = { resets: -1, commands: [], fullCommands: [], files: [] }
 
 function getContext (state = initialState) {
@@ -13,24 +9,18 @@ function incrementResets (state = initialState) {
   return Object.assign({}, state, {resets: state.resets + 1})
 }
 
-<<<<<<< HEAD
-=======
 function addFileToContext (state = initialState, file) {
   return Object.assign({}, state, { files: state.files.slice().push(file) })
 }
 
->>>>>>> I-1150: Add redux state management for usage.js.
 function contextReducer (state = initialState, action = {}) {
   switch (action.type) {
     case GET_CONTEXT:
       return getContext(state)
     case INCREMENT_CONTEXT_RESETS:
       return incrementResets(state)
-<<<<<<< HEAD
-=======
     case ADD_FILE_TO_CONTEXT:
       return addFileToContext(state, action.file)
->>>>>>> I-1150: Add redux state management for usage.js.
     default:
       return state
   }
