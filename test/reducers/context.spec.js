6976bbfd62de7dbb54cf5a0fe0400b69f7cffe9e
Aori Nevo <aori.nevo@bloomingdales.com>
2018-06-18 22:09:43 -0400

    I-1150: Phase 3 - Resolve comments.

--- test/reducers/context.spec.js
+++ test/reducers/context.spec.js
@@ -5,7 +5,7 @@ require('chai').should()
 const expect = require('chai').expect
 
 describe('context reducer', () => {
-  const { getContext, incrementContextResets } = require('../../actions/context')
+  const { getContext, incrementContextResets, addFileToContext } = require('../../actions/context')
   const contextReducer = require('../../reducers/context')
   it('should get context', () => {
     Object.keys(contextReducer(undefined, getContext())).should.have.length(4)
@@ -13,5 +13,9 @@ describe('context reducer', () => {
 
   it('should increment resets by 1', () => {
     expect(contextReducer(undefined, incrementContextResets()).resets).to.equal(0)
+  })
+
+  it('should add file to files', () => {
+    expect(contextReducer(undefined, addFileToContext('some/file/path')).files.length).to.equal(1)
   })
 })
