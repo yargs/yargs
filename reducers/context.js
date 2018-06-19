b08edc891e9523cc13f29d9fd39c48c4ec9fc45d
Aori Nevo <aori.nevo@bloomingdales.com>
2018-06-18 22:09:43 -0400

    I-1150: Phase 3 - Resolve comments.

--- reducers/context.js
+++ reducers/context.js
@@ -1,22 +1,16 @@
-const {GET_CONTEXT, INCREMENT_CONTEXT_RESETS, ADD_FILE_TO_CONTEXT} = require('../actions/context')
+const {INCREMENT_CONTEXT_RESETS, ADD_FILE_TO_CONTEXT} = require('../actions/context')
 const initialState = { resets: -1, commands: [], fullCommands: [], files: [] }
-
-function getContext (state = initialState) {
-  return state
-}
 
 function incrementResets (state = initialState) {
   return Object.assign({}, state, {resets: state.resets + 1})
 }
 
 function addFileToContext (state = initialState, file) {
-  return Object.assign({}, state, { files: state.files.slice().push(file) })
+  return Object.assign({}, state, { files: [...state.files, file] })
 }
 
 function contextReducer (state = initialState, action = {}) {
   switch (action.type) {
-    case GET_CONTEXT:
-      return getContext(state)
     case INCREMENT_CONTEXT_RESETS:
       return incrementResets(state)
     case ADD_FILE_TO_CONTEXT:
