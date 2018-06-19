554c09e8fb4d24a81be49ce18178a5517cdf0776
Aori Nevo <aori.nevo@bloomingdales.com>
2018-06-18 22:12:47 -0400

    I-1150: Phase 3 - Breakout changes for context into a separate branch.

--- reducers/index.js
+++ reducers/index.js
@@ -1,10 +1,8 @@
 const { combineReducers } = require('redux')
 const middlewareReducer = require('../reducers/middleware')
-const contextReducer = require('../reducers/context')
 const usageReducer = require('../reducers/usage')
 
 module.exports = combineReducers({
   middleware: middlewareReducer,
-  context: contextReducer,
   usage: usageReducer
 })
