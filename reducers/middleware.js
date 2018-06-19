92be54a02d8f43f26f33fb9e2327eb26baa0a348
Aori Nevo <aori.nevo@bloomingdales.com>
2018-06-18 22:09:43 -0400

    I-1150: Phase 3 - Resolve comments.

--- reducers/middleware.js
+++ reducers/middleware.js
@@ -1,4 +1,4 @@
-const {ADD_MIDDLEWARE, GET_MIDDLEWARE, RESET_MIDDLEWARE} = require('../actions/middleware')
+const {ADD_MIDDLEWARE, RESET_MIDDLEWARE} = require('../actions/middleware')
 const initialState = []
 
 function addMiddleware (state = initialState, middleware) {
@@ -9,18 +9,12 @@ function addMiddleware (state = initialState, middleware) {
   }
 }
 
-function getMiddleware (state = initialState) {
-  return state
-}
-
 function resetMiddleware (state = initialState) {
   return []
 }
 
 function middlewareReducer (state = initialState, action = {}) {
   switch (action.type) {
-    case GET_MIDDLEWARE:
-      return getMiddleware(state)
     case ADD_MIDDLEWARE:
       return addMiddleware(state, action.middleware)
     case RESET_MIDDLEWARE:
