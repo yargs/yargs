aefb6763e49987f1d596318e57144e0f597667a7
Aori Nevo <aori.nevo@bloomingdales.com>
2018-07-15 11:38:41 -0400

    I-1150: Migrated defaultCommand and aliasMap to Redux.

--- actions/command.js
+++ actions/command.js
@@ -1,12 +1,9 @@
+const RESET_COMMAND = 'RESET_COMMAND'
+const FREEZE_COMMAND = 'FREEZE_COMMAND'
+const UNFREEZE_COMMAND = 'UNFREEZE_COMMAND'
 const ADD_COMMAND_HANDLERS = 'ADD_COMMAND_HANDLERS'
-const ADD_COMMAND = 'ADD_COMMAND'
-
-function addCommand (value) {
-  return {
-    type: ADD_COMMAND,
-    value
-  }
-}
+const SET_DEFAULT_COMMAND = 'SET_DEFAULT_COMMAND'
+const SET_ALIAS_MAP = 'SET_ALIAS_MAP'
 
 function addCommandHandlers (parsedCommandCmd, cmd, description, handler, builder = {}, middlewares = [], demanded, optional) {
   const {spawnSync} = require('child_process')
@@ -18,7 +15,7 @@ function addCommandHandlers (parsedCommandCmd, cmd, description, handler, builde
     type: ADD_COMMAND_HANDLERS,
     key: parsedCommandCmd,
     value: {
-      cmd,
+      original: cmd,
       description,
       handler,
       builder,
@@ -29,9 +26,50 @@ function addCommandHandlers (parsedCommandCmd, cmd, description, handler, builde
   }
 }
 
+function setDefaultCommand (key) {
+  return {
+    type: SET_DEFAULT_COMMAND,
+    key
+  }
+}
+
+function setAliasMap (key, value) {
+  return {
+    type: SET_ALIAS_MAP,
+    key,
+    value
+  }
+}
+
+function resetCommand () {
+  return {
+    type: RESET_COMMAND
+  }
+}
+
+function freezeCommand () {
+  return {
+    type: FREEZE_COMMAND
+  }
+}
+
+function unfreezeCommand () {
+  return {
+    type: UNFREEZE_COMMAND
+  }
+}
+
 module.exports = {
-  ADD_COMMAND_HANDLERS,
-  ADD_COMMAND,
   addCommandHandlers,
-  addCommand
+  setDefaultCommand,
+  setAliasMap,
+  resetCommand,
+  freezeCommand,
+  unfreezeCommand,
+  ADD_COMMAND_HANDLERS,
+  SET_DEFAULT_COMMAND,
+  SET_ALIAS_MAP,
+  RESET_COMMAND,
+  FREEZE_COMMAND,
+  UNFREEZE_COMMAND
 }
--- reducers/command.js
+++ reducers/command.js
@@ -1,17 +1,74 @@
-const { ADD_COMMAND_HANDLERS } = require('../actions/command')
+const {
+  ADD_COMMAND_HANDLERS,
+  SET_DEFAULT_COMMAND,
+  SET_ALIAS_MAP,
+  RESET_COMMAND,
+  FREEZE_COMMAND,
+  UNFREEZE_COMMAND
+} = require('../actions/command')
 const initialState = {
   handlers: {},
+  aliasMap: {},
   defaultCommand: undefined
 }
 
 function addCommandHandlers (state = initialState, key, value) {
+  // const test = Object.assign({}, state, {handlers: {...JSON.parse(JSON.stringify(state.handlers)), [key]: value}})
+  // console.log('action command: ', JSON.stringify(test))
   return Object.assign({}, state, {handlers: {...state.handlers, [key]: value}})
+}
+
+function setDefaultCommand (state = initialState, key) {
+  // const test = Object.assign({}, state, {handlers: {...JSON.parse(JSON.stringify(state.handlers)), [key]: value}})
+  // console.log('action command: ', JSON.stringify(test))
+  return {...state, defaultCommand: state.handlers[key]}
+}
+
+function setAliasMap (state = initialState, key, value) {
+  // const test = Object.assign({}, state, {handlers: {...JSON.parse(JSON.stringify(state.handlers)), [key]: value}})
+  // console.log('action command: ', JSON.stringify(test))
+  return {...state, aliasMap: {...state.aliasMap, [key]: value}}
+}
+
+function resetCommand (state = initialState) {
+  return {
+    handlers: {},
+    aliasMap: {},
+    defaultCommand: undefined,
+    frozen: state.frozen
+  }
+}
+
+function freezeCommand (state = initialState) {
+  return Object.assign({}, state, {
+    frozen: {
+      handlers: {...state.handlers},
+      aliasMap: {...state.aliasMap},
+      defaultCommand: state.defaultCommand
+    }
+  })
+}
+
+function unfreezeCommand (state = initialState) {
+  const nextState = Object.assign({}, state, state.frozen)
+  delete nextState.frozen
+  return nextState
 }
 
 module.exports = function commandReducer (state = initialState, action) {
   switch (action.type) {
     case ADD_COMMAND_HANDLERS:
       return addCommandHandlers(state, action.key, action.value)
+    case SET_DEFAULT_COMMAND:
+      return setDefaultCommand(state, action.key)
+    case SET_ALIAS_MAP:
+      return setAliasMap(state, action.key, action.value)
+    case RESET_COMMAND:
+      return resetCommand(state)
+    case FREEZE_COMMAND:
+      return freezeCommand(state)
+    case UNFREEZE_COMMAND:
+      return unfreezeCommand(state)
     default:
       return state
   }
