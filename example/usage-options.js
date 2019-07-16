51c1f5a973acc1e3cd9890eef2b90565d01c6953
AK <52356853+aakti@users.noreply.github.com>
2019-07-17 00:35:14 +0530

    Fix: Usage-options example #1176

--- example/usage-options.js
+++ example/usage-options.js
@@ -1,17 +1,18 @@
 var yargs = require('yargs');
 
-var argv = yargs.usage('This is my awesome program', {
-  'about': {
-    description: 'Provide some details about the author of this program',
-    required: true,
-    alias: 'a',
-  },
-  'info': {
-    description: 'Provide some information about the node.js agains!!!!!!',
-    boolean: true,
-    alias: 'i'
-  }
-}).argv;
+var argv = yargs.usage('This is my awesome program')
+  .options({
+    'about': {
+      description: 'Provide some details about the author of this program',
+      required: true,
+      alias: 'a',
+    },
+    'info': {
+      description: 'Provide some information about the node.js agains!!!!!!',
+      boolean: true,
+      alias: 'i'
+    }
+  }).argv;
 
 yargs.showHelp();
 
