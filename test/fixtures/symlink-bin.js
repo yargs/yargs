0d3642b6f829b637938774c0c6ce5f6bfe1afa51
Benjamin E. Coe <bencoe@google.com>
2019-10-26 19:21:26 -0700

    refactor!: remove package.json-based parserConfiguration (#1460)

--- test/fixtures/symlink-bin.js
+++ test/fixtures/symlink-bin.js
@@ -2,5 +2,9 @@
 var argv = require('./yargs-symlink/index.js')
   .help('help')
   .version()
+  .parserConfiguration({
+    'dot-notation': false,
+    'boolean-negation': false
+  })
   .argv
 console.log(argv)
