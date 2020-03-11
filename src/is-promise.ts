fa322291d5115c0020a93d0e411ebf8b5f8fb1ff
Mael LE GUEN <mael.leguen@laposte.fr>
2020-03-11 12:42:28 +0100

    chore(ts): init typescript conf + tsify is-promise

--- src/is-promise.ts
+++ src/is-promise.ts
@@ -1,3 +1,5 @@
-module.exports = function isPromise (maybePromise) {
-  return !!maybePromise && !!maybePromise.then && (typeof maybePromise.then === 'function')
+export function isPromise<T> (maybePromise: T | Promise<T>): maybePromise is Promise<T> {
+  return !!maybePromise &&
+    !!(maybePromise as Promise<T>).then &&
+    (typeof (maybePromise as Promise<T>).then === 'function')
 }
--- test/is-promise.js
+++ test/is-promise.js
@@ -2,7 +2,7 @@
 /* global describe, it */
 
 const { expect } = require('chai')
-const isPromise = require('../lib/is-promise')
+const { isPromise } = require('../dist/is-promise')
 
 describe('isPromise', () => {
   it('returns `false` on non promise value', () => {
