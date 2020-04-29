5bba86442b90bae2852b9c50418501fe16740d21
Mael Le Guen <github@mlg.lgnby.fr>
2020-04-29 14:58:17 +0200

    Group validation type definitions

--- lib/types/frozen-validation-instance.ts
+++
@@ -1,9 +0,0 @@
-import { CustomCheck } from './custom-check'
-import { Dictionary } from './dictionary'
-import { KeyOrPos } from './key-or-pos'
-
-export interface FrozenValidationInstance {
-  implied: Dictionary<KeyOrPos[]>
-  checks: CustomCheck[]
-  conflicting: Dictionary<(string | undefined)[]>
-}
--- lib/validation.ts
+++ lib/validation.ts
@@ -1,9 +1,9 @@
 import { argsert } from './argsert'
 import { levenshtein as distance } from './levenshtein'
 import { objFilter } from './obj-filter'
-import { UsageInstance, ValidationInstance, Dictionary, CustomCheck, KeyOrPos, FrozenValidationInstance } from './types'
+import { UsageInstance, Dictionary } from './types'
 import { YargsInstance } from './types/yargs'
-import { Arguments } from 'yargs-parser'
+import { Arguments, DetailedArguments } from 'yargs-parser'
 import Y18N = require('y18n')
 const specialKeys = ['$0', '--', '_']
 
@@ -402,3 +402,41 @@ export function validation (yargs: YargsInstance, usage: UsageInstance, y18n: Y1
 
   return self
 }
+
+/** Instance of the validation module. */
+interface ValidationInstance {
+  check(f: CustomCheck['func'], global: boolean): void
+  conflicting(argv: Arguments): void
+  conflicts(key: string, value: string | string[]): void
+  conflicts(key: Dictionary<string | string[]>): void
+  customChecks(argv: Arguments, aliases: DetailedArguments['aliases']): void
+  freeze(): void
+  getConflicting(): Dictionary<(string | undefined)[]>
+  getImplied(): Dictionary<KeyOrPos[]>
+  implications(argv: Arguments): void
+  implies(key: string, value: KeyOrPos | KeyOrPos[]): void
+  implies(key: Dictionary<KeyOrPos | KeyOrPos[]>): void
+  isValidAndSomeAliasIsNotNew(key: string, aliases: DetailedArguments['aliases']): boolean
+  limitedChoices(argv: Arguments): void
+  nonOptionCount(argv: Arguments): void
+  positionalCount(required: number, observed: number): void
+  recommendCommands(cmd: string, potentialCommands: string[]): void
+  requiredArguments(argv: Arguments): void
+  reset(localLookup: Dictionary): ValidationInstance
+  unfreeze(): void
+  unknownArguments(argv: Arguments, aliases: DetailedArguments['aliases'], positionalMap: Dictionary): void
+  unknownCommands(argv: Arguments): boolean
+}
+
+interface CustomCheck {
+  func: (argv: Arguments, aliases: DetailedArguments['aliases']) => any
+  global: boolean
+}
+
+interface FrozenValidationInstance {
+  implied: Dictionary<KeyOrPos[]>
+  checks: CustomCheck[]
+  conflicting: Dictionary<(string | undefined)[]>
+}
+
+type KeyOrPos = string | number | undefined
