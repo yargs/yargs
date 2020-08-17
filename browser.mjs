58846b2ad393acacd8b1845e129ec35aabdfbfa0
bcoe <bencoe@google.com>
2020-08-16 19:12:32 -0700

    chore: fix import

--- browser.mjs
+++ browser.mjs
@@ -1,5 +1,5 @@
 // Bootstrap yargs for browser:
-import browserPlatformShim from './lib/platform-shims/browser.js'
+import browserPlatformShim from './lib/platform-shims/browser.mjs'
 import { YargsWithShim } from './build/lib/yargs-factory.js'
 
 const Yargs = YargsWithShim(browserPlatformShim)
