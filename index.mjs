803a7d8a67454137b9be1c70c686f35b257e67a4
bcoe <bencoe@google.com>
2020-08-18 22:20:52 -0700

    feat: expose helper for process args

--- index.mjs
+++ index.mjs
@@ -2,8 +2,12 @@
 
 // Bootstraps yargs for ESM:
 import esmPlatformShim from './lib/platform-shims/esm.mjs'
+import { getProcessArgvWithoutBin } from './build/lib/utils/process-argv.js'
 import { YargsWithShim } from './build/lib/yargs-factory.js'
 
 const Yargs = YargsWithShim(esmPlatformShim)
 
-export default Yargs
+export {
+  getProcessArgvWithoutBin,
+  Yargs,
+}
--- test/esm/yargs-test.mjs
+++ test/esm/yargs-test.mjs
@@ -1,25 +1,31 @@
 /* global describe, it */
 
 import * as assert from 'assert'
-import yargs from '../../index.mjs'
+import { Yargs, getProcessArgvWithoutBin } from '../../index.mjs'
 
 describe('ESM', () => {
   describe('parser', () => {
     it('parses process.argv by default', () => {
-      const parsed = yargs(['--goodnight', 'moon']).parse()
+      const parsed = Yargs(['--goodnight', 'moon']).parse()
       assert.strictEqual(parsed.goodnight, 'moon')
     })
-    // TODO: add test for hiding process.argv arguments.
   })
   describe('commandDir',  () => {
     it('throws an error if commndDir() used in ESM mode', () => {
       let err;
       try {
-        yargs().commandDir('./')
+        Yargs().commandDir('./')
       } catch (_err) {
         err = _err
       }
       assert.match(err.message, /not supported yet for ESM/)
+    })
+  })
+  describe('getProcessArgvWithoutBin', () => {
+    it('hides bin for standard node.js application', () => {
+      process.argv = ['node', 'foo.js', '--apple', '--banana']
+      const args = getProcessArgvWithoutBin()
+      assert.deepEqual(args, ['--apple', '--banana'])
     })
   })
 })
