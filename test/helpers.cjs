c7debe8eb1e5bc6ea20b5ed68026c56e5ebec9e1
Benjamin E. Coe <bencoe@google.com>
2020-09-07 11:20:40 -0700

    feat(helpers): rebase, Parser, applyExtends now blessed helpers (#1733)

---
+++ test/helpers.cjs
@@ -0,0 +1,27 @@
+'use strict'
+
+const {describe, it} = require('mocha')
+const assert = require('assert')
+const yargs = require('../yargs.cjs');
+const { applyExtends } = require('../yargs.cjs');
+
+const HELPER_COUNT = 2;
+
+describe('helpers', () => {
+  it('does not expose additional helpers beyond blessed list', () => {
+    assert.strictEqual(Object.keys(yargs).length, HELPER_COUNT)
+  })
+  describe('applyExtends', () => {
+    it('exposes applyExtends helper', () => {
+      const conf = applyExtends({extends: './package.json', apple: 'red'}, process.cwd(), true);
+      assert.strictEqual(conf.name, 'yargs') // loads packge.json.
+      assert.strictEqual(conf.apple, 'red') // keeps config with extends.
+    })
+  })
+  describe('Parser', () => {
+    it('exposes functional argument parser', () => {
+      const argv = yargs.Parser('--foo --bar=99')
+      assert.strictEqual(argv.bar, 99)
+    })
+  })
+})
