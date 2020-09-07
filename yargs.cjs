5081eb117d557de2f1fb2143850800637858e879
bcoe <bencoe@google.com>
2020-09-07 10:53:27 -0700

    docs: correct comment

--- yargs.cjs
+++ yargs.cjs
@@ -1,7 +1,6 @@
-// CJS import shim for older versions of Node.js.
-// this can be removed once all supported Node.js versions support
-// export maps:
-const {applyExtends, cjsPlatformShim, Parser, rebase, Yargs} = require('./build/index.cjs')
+// TODO: consolidate on using a helpers file at some point in the future, which
+// is the approach currently used to export Parser and  applyExtends for ESM:
+const {applyExtends, cjsPlatformShim, Parser, Yargs} = require('./build/index.cjs')
 Yargs.applyExtends = (config, cwd, mergeExtends) => {
   return applyExtends(config, cwd, mergeExtends, cjsPlatformShim)
 }
