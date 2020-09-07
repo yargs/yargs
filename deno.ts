f1d7d15783580335bed67ca55a664acc7fe3d818
bcoe <bencoe@google.com>
2020-09-07 12:17:23 -0700

    chore: do not rename import

--- deno.ts
+++ deno.ts
@@ -1,11 +1,11 @@
 // Bootstrap yargs for Deno platform:
 import denoPlatformShim from './lib/platform-shims/deno.ts'
 import { YargsWithShim } from './build/lib/yargs-factory.js'
-import { YargsInstance as YargsType } from './build/lib/yargs-factory.d.ts'
+import { YargsInstance } from './build/lib/yargs-factory.d.ts'
 
 const WrappedYargs = YargsWithShim(denoPlatformShim)
 
-function Yargs (args?: string[]): YargsType {
+function Yargs (args?: string[]): YargsInstance {
   return WrappedYargs(args)
 }
 
