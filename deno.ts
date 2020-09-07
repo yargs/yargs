79c969693883aeb6d569ec537c4aca820e2d78fd
bcoe <bencoe@google.com>
2020-09-07 12:24:31 -0700

    chore: address bug with linter

--- deno.ts
+++ deno.ts
@@ -1,7 +1,7 @@
 // Bootstrap yargs for Deno platform:
 import denoPlatformShim from './lib/platform-shims/deno.ts'
 import { YargsWithShim } from './build/lib/yargs-factory.js'
-import { YargsInstance } from './build/lib/yargs-factory.d.ts'
+import { YargsInstance } from './build/lib/yargs-factory.d.ts' // eslint-disable-line
 
 const WrappedYargs = YargsWithShim(denoPlatformShim)
 
