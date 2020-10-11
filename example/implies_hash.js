f7f4b590eb5be288e540f1b60943155fc806f3a5
bcoe <bencoe@google.com>
2020-10-11 16:32:33 -0700

    docs: move all examples away from singleton

--- example/implies_hash.js
+++ example/implies_hash.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-var argv = require('yargs')
+var argv = require('yargs/yargs')(process.argv.slice(2))
     .usage('Usage: $0 -x [num] -y [num] -w [msg] -h [msg]')
     .implies({
         x: 'y',
--- example/string.js
+++ example/string.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-var argv = require('yargs')
+var argv = require('yargs/yargs')(process.argv.slice(2))
     .string('x', 'y')
     .argv
 ;
