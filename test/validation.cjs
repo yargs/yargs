b063c4422692cb1e2a4d782f8a9f63b45ce8dc9a
Landon Yarrington <jly.dev.36963@gmail.com>
2021-05-30 23:16:29 -0600

    fix: conflicts and strip-dashed

--- test/validation.cjs
+++ test/validation.cjs
@@ -190,6 +190,28 @@ describe('validation tests', () => {
         .parse();
     });
 
+    // strip-dash breaks conflicting
+    // https://github.com/yargs/yargs/issues/1952
+    it('fails if conflicting arguments are provided, and strip-dash is enabled', () => {
+      yargs()
+        .option('foo-foo', {
+          description: 'a foo-foo',
+          type: 'string',
+        })
+        .option('bar-bar', {
+          description: 'a bar-bar',
+          type: 'string',
+        })
+        .conflicts({'foo-foo': 'bar-bar'})
+        .parserConfiguration({'strip-dashed': true})
+        .parse('--foo-foo a --bar-bar b', (error, argv, output) => {
+          expect(error).to.not.equal(null);
+          error.message.should.match(
+            /Arguments foo-foo and bar-bar are mutually exclusive/
+          );
+        });
+    });
+
     it('should not fail if no conflicting arguments are provided', () => {
       yargs(['-b', '-c'])
         .conflicts('f', ['b', 'c'])
