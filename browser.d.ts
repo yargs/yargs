5fd309c8094a7213c7985357d978bb978e7c5d21
Max Isom <hi@maxisom.me>
2022-03-08 10:01:47 -0500

    Fix declare

--- browser.d.ts
+++ browser.d.ts
@@ -1,5 +1,5 @@
 import { YargsFactory } from "./build/lib/yargs-factory";
 
-const Yargs: ReturnType<typeof YargsFactory>
+declare const Yargs: ReturnType<typeof YargsFactory>
 
 export default Yargs;
