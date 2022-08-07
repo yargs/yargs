84ad2f15b9a7a1f391503265fe683743954cfaf2
Federico Ciardi <fed.ciardi@gmail.com>
2022-08-07 19:35:18 +0200

    test: improve coverage

--- lib/fig-completion.ts
+++ lib/fig-completion.ts
@@ -136,9 +136,7 @@ export class FigCompletion {
         choices: choices[option],
         count: count.includes(option),
         default: defaultValue[option],
-        demand: Object.keys(demandedOptions).includes(option)
-          ? demandedOptions[option] ?? true
-          : undefined,
+        demand: Object.keys(demandedOptions).includes(option) || undefined,
         deprecate: Object.keys(deprecatedOptions).includes(option)
           ? deprecatedOptions[option] ?? true
           : undefined,
