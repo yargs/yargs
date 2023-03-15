dfa441f159349833d07dc1c7c569ddd1541e0b9f
pengbo43 <pengbo43@jd.com>
2023-03-15 14:34:42 +0800

    chore: lint fix

--- lib/yargs-factory.ts
+++ lib/yargs-factory.ts
@@ -1293,7 +1293,11 @@ export class YargsInstance {
     level: 'error' | 'log' | ((message: string) => void),
     callback = () => {}
   ): YargsInstance {
-    argsert('[string|function] [function]', [level, callback], arguments.length);
+    argsert(
+      '[string|function] [function]',
+      [level, callback],
+      arguments.length
+    );
     this.#hasOutput = true;
     if (!this.#usage.hasCachedHelpMessage()) {
       if (!this.parsed) {
