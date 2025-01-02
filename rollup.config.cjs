188e0594312a91afb951f7a39cdee6c84d504152
Benjamin Coe <bencoe@gmail.com>
2025-01-02 14:27:46 -0500

    fix(build): address problems with typescript compilation

--- rollup.config.cjs
+++ rollup.config.cjs
@@ -1,5 +1,5 @@
 const cleanup = require('rollup-plugin-cleanup');
-const ts = require('rollup-plugin-ts');
+const ts = require('@rollup/plugin-typescript');
 const terser = require('@rollup/plugin-terser');
 
 const output = {
